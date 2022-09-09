1c26c26857d6a882df0e77088f12eaa8db4116f3
NdakoAdama <adamamuhammad01@gmail.com>
2022-09-09 16:02:02 -0700

    Task 7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -2,7 +2,7 @@
 /**
  * main - prints the lowercase alphabet in reverse
  *
- * Descript:A program that prints the lowercase alphabet in reverse 
+ * Descript: prints the lowercase alphabet in reverse 
  * it is executed.
  * Return: Always 0 (Success)
  */
