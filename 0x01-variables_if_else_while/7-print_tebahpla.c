ec45249a35e766831b2c40ddf533675232bef4f6
NdakoAdama <adamamuhammad01@gmail.com>
2022-09-09 16:25:11 -0700

    Task 7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -2,17 +2,18 @@
 /**
  * main - prints the lowercase alphabet in reverse
  *
- * Description:print the lowercase alphabet in reverse 
+ * Description:print the lowercase alphabet in revers 
  * it is executed.
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	char low;
-77
-	for (low = 'z'; low >= 'a'; low--)
-		putchar(low);
-	putchar('\n');
 
+	for (low = 'z'; low >= 'a'; low--)
+	{
+		putchar(low);
+	}
+	putchar('\n');
 	return (0);
 }
