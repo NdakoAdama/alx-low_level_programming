781713794fd0f959cecb3a566777d72ffbb6b229
NdakoAdama <adamamuhammad01@gmail.com>
2022-09-09 18:09:00 -0700

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,32 +7,29 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-int main(void)
-{
-	int d, p, q;
-
-	for (d = '0'; d < '9'; d++)
 	{
-		for (p = d + 1; p <= '9'; p++)
+	int n, m, l;
+
+	for (n = 48; n < 58; n++)
+	{
+		for (m = 49; m < 58; m++)
 		{
-			for (q = p + 1; q <= '9'; q++)
+			for (l = 50; l < 58; l++)
 			{
-				if ((p != d) != q)
+				if (l > m && m > n)
 				{
-					putchar(d);
-					putchar(p);
-					putchar(q);
-
-			if (d == '7' && p == '8')
-					continue;
-
-				putchar(',');
-				putchar(' ');
+					putchar(n);
+					putchar(m);
+					putchar(l);
+			if (n != 55 || m != 56)
+					{
+					putchar(',');
+					putchar(' ');
+					}
 				}
 			}
 		}
 	}
 	putchar('\n');
-
 	return (0);
 }
