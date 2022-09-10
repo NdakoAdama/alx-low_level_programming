8b583379db2dcde6330943aca31abd4a489f45e9
NdakoAdama <adamamuhammad01@gmail.com>
2022-09-09 18:41:09 -0700

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -22,9 +22,9 @@ int main(void)
 					putchar(m);
 					putchar(l);
 			if (n != 55 || m != 56)
-					{
-					putchar(',');
-					putchar(' ');
+			{
+				putchar(',');
+				putchar(' ');
 					}
 				}
 			}
