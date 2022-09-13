ec4583d1b791e3493a1f5ec47245ddd8d9630ea8
NdakoAdama <adamamuhammad01@gmail.com>
2022-09-13 16:22:52 -0700

    Task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,41 +1,20 @@
 #include"main.h"
 
 /**
- * print_times_table - prints time table of n
+ * print_to_98 - print n to 98 counts separated by comma, followed by space and number should be printed in order
  *
- * @n: takes number input
-*/
+ * @n: input
+ */
 
-void print_times_table(int n)
+void print_to_98(int n)
 {
-	int prod, mult, num;
+	int count;
 
-	if (n <= 15 && n >= 0)
-	{
-		for (num = 0; num <= n; ++num)
-		{
-			_putchar(48);
-			for (mult = 1; mult <= n; ++mult)
-			{
-				_putchar(',');
-				_putchar(' ');
-
-				prod = num * mult;
-
-				if (prod <= 9)
-					_putchar(' ');
-				if (prod <= 99)
-					_putchar(' ');
-
-				if (prod >= 100)
-				{
-					_putchar((prod / 100) + 48);
-					_putchar((prod / 10) % 10 + 48);
-				} else if (prod <= 99 && prod >= 10)
-					_putchar((prod / 10) + 48);
-				_putchar((prod % 10) + 48);
-			}
-			_putchar('\n');
-		}
-	}
+	if (n > 98)
+		for (count = n; count > 98; --count)
+			printf("%d, ", count);
+	else
+		for (count = n; count < 98; ++count)
+			printf("%d, ", count);
+	printf("98\n");
 }
