4a36242ff6a5f78990fccfffe0564c12675c12f3
NdakoAdama <adamamuhammad01@gmail.com>
2022-09-15 15:07:12 -0700

    Task 0

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,17 +1,21 @@
-#ifndef main.h
-#define main.h
+#include <unistd.h>
 
+/**
+*_putchar - writes the character c to stdout
+*@c: The character to print
+*
+*Return: In success 1.
+*On error, -1 is returned, and errno is set appropriately.
+*/
 int _putchar(char c);
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
-void print_numbers(void);
-void print_most_numbers(void);
-void more_numbers(void);
-void print_line(int n);
-void print_diagonal(int n);
-void print_square(int size);
-void print_triangle(int size);
-void print_number(int n);
-
-#endif /* main.h */
+void print_alphabet();
+void print_alphabet_x10();
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int n);
+int print_last_digit(int a);void jack_bauer();
+void times_table(void);
+int add(int j, int k);
+void print_to_98(int a);
+void print_times_table(int n);
