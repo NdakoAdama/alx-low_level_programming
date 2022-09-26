8eb0e7cf985d67dae6067b141dff263a34c6d92e
NdakoAdama <adamamuhammad01@gmail.com>
2022-09-26 09:46:56 -0700

    Task 1

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,11 +1,11 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * _memset - a function that fills memory with constant bytes
- * @s: location to fill
+ * @s: memory area
  * @b: char to fill location with
  * @n: number of bytes to fill
- * Return: returns pointer to location filled
+ * Return: returns pointer to memory area
  */
 char *_memset(char *s, char b, unsigned int n)
 {
