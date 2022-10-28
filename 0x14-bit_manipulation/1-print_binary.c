15bf4943f1bc907f6c9ef1c2d710cf5de40727a9
NdakoAdama <adamamuhammad01@gmail.com>
2022-10-27 21:33:57 -0700

    Task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,24 +1,46 @@
-#include "main.h"
+#include <stdio.h>
 /**
-  *print_binary - prints the binary representation of a number.
-  *@n: unsigned long int to be converted.
-  *
-  *Return: void.
-  */
+ * print_binary - prints decimal as binary
+ * @n: long integer
+ */
+
 void print_binary(unsigned long int n)
 {
-	unsigned long int mask;
+	signed long int size;
+	char c;
+	int flag;
+
+	size = sizeof(n) * 8 - 1;
 
 	if (n == 0)
 	{
-		_putchar('0');
+		printf("0");
+		return;
 	}
-	else
+
+	if (n == 1)
 	{
-		mask = n & 1;
-		n = n >> 1;
-		if (!(n == 0))
-			print_binary(n);
-		_putchar(mask + '0');
+		printf("1");
+		return;
+	}
+
+	flag = 0;
+
+	while (size >= 0)
+	{
+		c = (n >> size) & 1;
+
+		if (flag == 1)
+			putchar(c + '0');
+		else
+		{
+			if (c == 1)
+			{
+				putchar(c + '0');
+				flag = 1;
+			}
+		}
+
+		size -= 1;
 	}
 }
