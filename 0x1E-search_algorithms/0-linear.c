019f09850ee749d7b7e90e75bcf06ddebd105543
NdakoAdama <adamamuhammad01@gmail.com>
2023-04-14 00:44:34 -0700

    task 0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -9,33 +9,18 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-    size_t i;
+	size_t i;
 
-    if (!array)
-        return (-1);
+	if (!array)
+		return (-1);
 
-    i = 0;
-    while (i < size)
-    {
-        printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-        if (array[i] == value)
-            return (i);
-        i++;
-    }
-    return (-1);
-}
-
-int main()
-{
-    int arr[] = { 1, 2, 3, 4, 5 };
-    int size = sizeof(arr) / sizeof(int);
-    int value = 3;
-    int index = linear_search(arr, size, value);
-
-    if (index == -1)
-        printf("%d not found in the array\n", value);
-    else
-        printf("%d found at index %d in the array\n", value, index);
-
-    return 0;
+	i = 0;
+	while (i < size)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+		i++;
+	}
+	return (-1);
 }
